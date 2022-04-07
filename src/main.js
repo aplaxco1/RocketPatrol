14a036d1914773a6d886e36768858f5188f0964d
aplaxco <aplaxco@ucsc.edu>
2022-04-06 19:23:02 -0700

    adding Rocket movement

--- src/main.js
+++ src/main.js
@@ -10,3 +10,6 @@ let game = new Phaser.Game(config);
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// keyboard keys
+let keyF, keyR, keyLEFT, keyRIGHT;
--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,5 +5,37 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         // add object to existing scene
         scene.add.existing(this);
+        // track rocket's firing status
+        this.isFiring = false;
+        // movement pixels per frame
+        this.moveSpeed = 2;
+    }
+
+    update() {
+        //left and right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } 
+            else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;  
+            } 
+        }
+
+        // fire button
+        if (Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
+        }
+
+        // if fired, move rocket up
+        if (this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+
+        // reset rocket to bottom on miss
+        if (this.y <= borderUISize * 3 + borderPadding) {
+            this.isFiring = false;
+            this.y = game.config.height - borderUISize - borderPadding;
+        }
     }
 }
